d8a0b0b7c6c4f4032380a86485b3d582795236e0
ho <ho.alimoradi@gmail.com>
2023-06-23 01:54:19 +0330

    added splash, common and coordinator structure

---
+++ Commons/Commons/Commons.h
@@ -0,0 +1,18 @@
+//
+//  Commons.h
+//  Commons
+//
+//  Created by hoseinAlimoradi on 6/23/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Commons.
+FOUNDATION_EXPORT double CommonsVersionNumber;
+
+//! Project version string for Commons.
+FOUNDATION_EXPORT const unsigned char CommonsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Commons/PublicHeader.h>
+
+
---
+++ Coordinator/Coordinator/Coordinator.h
@@ -0,0 +1,18 @@
+//
+//  Coordinator.h
+//  Coordinator
+//
+//  Created by hoseinAlimoradi on 6/23/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Coordinator.
+FOUNDATION_EXPORT double CoordinatorVersionNumber;
+
+//! Project version string for Coordinator.
+FOUNDATION_EXPORT const unsigned char CoordinatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Coordinator/PublicHeader.h>
+
+
